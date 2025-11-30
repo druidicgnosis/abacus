e25bd4416074c73f40d0d3556dee226d6dc9277a
mike <druidinsanity@proton.me>
2025-11-30 10:49:54 +0000

    cheated but got carry animation working

--- abanimation.c
+++ abanimation.c
@@ -4,8 +4,28 @@
 
 #include "ablib.h"
 
-//static char termlate[19] =
-//{'|','*','*','*','*','*','*','*','*','*','-','-','-','-','-','-','-','|', '\0',};
+int carryAnim(char **abacus, int line) {
+
+	int len = 0;
+	while ((*abacus)[len] != '\0') len++;
+	int line_start = len - (LINELEN * line) + 1;
+	int line_end = line_start + LINELEN - 2;	// why. ts jst linestart + LINELEN -2
+
+	int j = 0;
+	for (int i = line_end - 1; i > line_start; i--) {
+		if ((*abacus)[i] == '*' && (*abacus)[i - 1] == '*' && ( (*abacus)[i + 1] == '-' || (*abacus)[i + 1] == '|' )) {
+				if (!j) j = i;
+		}
+		if ((*abacus)[i] == '-' && (*abacus)[i + 1] == '*' && j) {
+			char tmp = (*abacus)[i];
+			(*abacus)[i] = (*abacus)[j];
+			(*abacus)[j] = tmp;
+			//free(temp);
+			return 0;
+		}
+	}
+	return 1;
+}
 
 int shiftRight(char **abacus, int line) {	// exits: 0 = completed move, 1 = error, 2 = successful partial move, 3 = carry creating newline
 
@@ -38,8 +58,7 @@ int shiftRight(char **abacus, int line) {	// exits: 0 = completed move, 1 = erro
 	}
 
 	if (!foundBead) {
-		//for (int i = line_start; i < line_end; i++) (*abacus)[i] = carryline[i - line_start];
-		for (int i = 0; i < 9; i++) moveLeft(abacus, line);
+		//for (int i = 0; i < 9; i++) moveLeft(abacus, line);
 		shiftRight(abacus, line + 1);
 		return 3;
 	}
@@ -55,16 +74,11 @@ int shiftRight(char **abacus, int line) {	// exits: 0 = completed move, 1 = erro
 	return 1;
 }
 
-//int shiftRightWrapper(char **abacus, int line) {
-//	int complete = 0;
-//
-//	while (!complete) {
-//		shiftRight(abacus, line);
-//}
-
 int main() {
 	bool done = false;
 	char *aba = initAbacus(LINELEN, 1);
+
+	printf("%s\n", "some bull shit that the animation shoul dnot overwrite");
 
 	printf("%s\n", aba);
 
@@ -72,28 +86,38 @@ int main() {
 	int p = 0;
 	int q = 1;
 	int powa = 10;
+	int caried = 0;
 	while (!done) {
 
-		usleep(10000);
+		usleep(30000);
 
 		printf("\033[%dA", q);
 
 		printf("\033[0J");
 
-		//if (shiftRight(&aba, 1) == 3) {
+		//if (shiftRight(&aba, 1) == 3) p += 10;
+		//printf("%s\n", aba);
+
+		//if (p > 0 && p % powa == 0) {
 		//	powa *= 10;
 		//	q++;
 		//}
-
-		if (shiftRight(&aba, 1) == 3) p += 10;
-		printf("%s\n", aba);
+		int eres = shiftRight(&aba, 1);
+		if (eres == 3) {
+			p += 10;
+			caried = 7;
+		}
+		//printf("%s %d\n", aba, q);
 
 		if (p > 0 && p % powa == 0) {
 			powa *= 10;
 			q++;
 		}
-		//if (eflag == 0) printf("%s complete\n", aba);
-		//else printf("%s\n", aba);
+		if (caried) {
+			carryAnim(&aba, q - 1);
+			--caried;
+		}
+		printf("%s q: %d, caried: %d\n", aba, q, caried);
 	}
 	printf("\n");
 }
